f58abcf0c0743467b44470b7c4e6f841011575ca
zozo-url <zozo-url@protonmail.ch>
2019-11-04 16:01:45 +1300

    Updates

---
+++ precache-manifest.fd0e753239277da650a48dd9d4e8cd89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb61a1ae8f117dd7b8678f45097297b8",
+    "url": "/bond-st/index.html"
+  },
+  {
+    "revision": "978ed3d66fff1ae96672",
+    "url": "/bond-st/static/js/2.4b816d6c.chunk.js"
+  },
+  {
+    "revision": "9c594b83b46b727eb894",
+    "url": "/bond-st/static/js/main.865891d3.chunk.js"
+  },
+  {
+    "revision": "fdd365db3ef8596241d0",
+    "url": "/bond-st/static/js/runtime-main.c5a7df94.js"
+  },
+  {
+    "revision": "abe035336d45da988bf286c703b46409",
+    "url": "/bond-st/static/media/logo-black.abe03533.png"
+  }
+]);
