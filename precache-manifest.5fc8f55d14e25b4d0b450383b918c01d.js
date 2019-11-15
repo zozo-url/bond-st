25d9dd58797dd79237593447e3a42b5d066555cc
zozo-url <zozo-url@protonmail.ch>
2019-11-15 16:41:30 +1300

    Updates

---
+++ precache-manifest.5fc8f55d14e25b4d0b450383b918c01d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35fa447c8c823901a4b5307d9b0f631d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "978ed3d66fff1ae96672",
+    "url": "/static/js/2.4b816d6c.chunk.js"
+  },
+  {
+    "revision": "9c594b83b46b727eb894",
+    "url": "/static/js/main.865891d3.chunk.js"
+  },
+  {
+    "revision": "ee5bfdd2c4e6da6c9388",
+    "url": "/static/js/runtime-main.a3384021.js"
+  },
+  {
+    "revision": "abe035336d45da988bf286c703b46409",
+    "url": "/static/media/logo-black.abe03533.png"
+  }
+]);
